749d508df92eed352b9b06c7b2579832c3a3f9c0
Mac <570768995@qq.com>
2018-09-05 17:50:49 +0800

    fix

--- build/wechatgame/game.js
+++ build/wechatgame/game.js
@@ -4,5 +4,5 @@ window.DOMParser = Parser.DOMParser;
 require('libs/wx-downloader.js');
 wxDownloader.REMOTE_SERVER_ROOT = "https://data.tianziyou.com/matchvsGamesRes/basketball";
 wxDownloader.SUBCONTEXT_ROOT = "";
-require('src/settings.5a9a6');
-require('main.f3772');
+require('src/settings.0aec5');
+require('main.ecc3a');
